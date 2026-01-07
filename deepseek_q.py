e67c9491f3772f12944ca9b8c48dd4a38829cca9
Sabika-Abbas <sabika.abbas5582@gmail.com>
2026-01-07 21:21:18 +0500

    tried "move zeroes to end" question

--- deepseek_q.py
+++ deepseek_q.py
@@ -26,7 +26,7 @@
 
 # write a function to reverse sort the array
 
-def reverse_sorting(arr):
+# def reverse_sorting(arr):
     # n=len(arr)
     # for i in range(n):
     #     for j in range(n-1-i):
@@ -34,14 +34,32 @@ def reverse_sorting(arr):
     #         print("j+1",arr[j+1])
     #         if arr[j]<arr[j+1]:
     #             arr[j],arr[j+1]=arr[j+1],arr[j]
+    # return arr
+# print(reverse_sorting([1,4,2,7,3,8]))
 
-    n=len(arr)
-    x=arr[0]
-    key=n//2
-    for i in range(n):
-        if x<arr[key]:
-            x,arr[key]=arr[key],x
-        key=key//2
-    return arr
+
+# Find second largest number
+
+# def second_largest(arr):
+#     largest=max(arr)
+#     arr_copy=arr.copy()
+#     arr_copy.remove(largest)
+#     second_largest=max(arr_copy)
+#     return second_largest
+# print(second_largest([43,65,12,54,44,37]))
+
+
+# move all zeroes to the end
+
+def move_zeroes_to_end(arr):
+    left=0
+    right=len(arr)-1
+    while left<=right:
         
-print(reverse_sorting([1,4,2,7,3,8]))
+        if arr[left]==0 and arr[right]!=0:
+            arr[left],arr[right]=arr[right],arr[left]
+            left+=1
+            right-=1
+    return arr
+print(move_zeroes_to_end([0,3,2,0,6,4]))
+        
